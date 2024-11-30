fa5c6e97d7517ed0aaf947dd2da409c4a7968091
lukmankb <lukmanbala29@gmail.com>
2024-11-30 22:47:24 +0100

    Module_Variables_Builtin_functions.py
    I did Level 1 Exercise of Module 2

--- 02_Module_Variables_Builtin_functions/Module_Variables_Builtin_functions.py
+++ 02_Module_Variables_Builtin_functions/Module_Variables_Builtin_functions.py
@@ -1,1 +1,14 @@
-
+# Exercises: Level 1
+#ArewaDS 30 Days of Python Module_02
+# Day 2: 30 Days of python programming
+first_name = 'Lukman'
+last_name = "Kabiru"
+fullName ='Lukman Kabiru Bala'
+country = "Nigeria"
+city = 'Kebbi'
+age = 21
+current_year = 2024
+is_married = False
+is_true = True
+is_light_on = True
+first_name, last_name, age, is_married = 'Lukman', 'Kabiru', 21, True 
