d6303df2e58d1e2cf3310f2f8de03551914dcbde
lukmankb <lukmanbala29@gmail.com>
2024-12-01 12:12:08 +0100

    Update Module_Variables_Builtin_functions.py
    i started exercise2 but did not finish

--- 02_Module_Variables_Builtin_functions/Module_Variables_Builtin_functions.py
+++ 02_Module_Variables_Builtin_functions/Module_Variables_Builtin_functions.py
@@ -12,3 +12,47 @@ is_married = False
 is_true = True
 is_light_on = True
 first_name, last_name, age, is_married = 'Lukman', 'Kabiru', 21, True 
+
+
+Check the data type of all your variables using type() built-in function
+print(type(first_name))
+print(type(last_name))
+print(type(fullName))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(current_year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+Using the len() built-in function, find the length of your first name
+print(len(first_name))
+Compare the length of your first name and your last name
+Declare 5 as num_one and 4 as num_two
+num_one, num_two = 5, 4
+Add num_one and num_two and assign the value to a variable total
+total = num_one + num_two
+Subtract num_two from num_one and assign the value to a variable diff
+diff = num_one - num_two
+Multiply num_two and num_one and assign the value to a variable product
+product = num_one * num_two
+Divide num_one by num_two and assign the value to a variable division
+division = num_one / num_two
+Use modulus division to find num_two divided by num_one and assign the value to a variable remainder
+remainder = num_two % num_one
+Calculate num_one to the power of num_two and assign the value to a variable exp
+exp = num_one ** num_two
+Find floor division of num_one by num_two and assign the value to a variable floor_division
+floor_division = num_one // num_two
+The radius of a circle is 30 meters.
+radius = 30, pi = 3.142
+Calculate the area of a circle and assign the value to a variable name of area_of_circle
+area_of_circle = pi * (radius ** 2)
+Calculate the circumference of a circle and assign the value to a variable name of circum_of_circle
+circum_of_Circe = 2 * pi * radius
+Take radius as user input and calculate the area.
+radius_input = input('Input Radius: ')
+area = pi * (radius_input ** 2)
+print(f'Area of Circle = {area}')
+Use the built-in input function to get first name, last name, country and age from a user and store the value to their corresponding variable names
+Run help('keywords') in Python shell or in your file to check for the Python reserved words or keywords
